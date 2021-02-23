21d36d46c523d46020bf940fbc4f7d35aa8989b2
Kevin See <Kevin.See@merck.com>
2021-02-22 14:39:57 -1000

    updated all simulation functions to use purrr rather than plyr

--- inst/scripts/SimFnc.R
+++ inst/scripts/SimFnc.R
@@ -9,7 +9,7 @@ SimulateLGRdata = function(N.lgr = 100000,
                            hnc.prob = .05,
                            n.pops = 25,
                            hatch.pop.prob = 0.5,
-                           hnc.pop.prob = 0.15,                           
+                           hnc.pop.prob = 0.15,
                            run.mu.mu = 171,
                            run.mu.sd = 13,
                            run.sd.mu = 22,
@@ -38,32 +38,32 @@ SimulateLGRdata = function(N.lgr = 100000,
   # ladder.rate: detection probability of PIT tag antenna in the ladder
   # start.date: date from which simulation begins
   # trap.rate.df: data.frame with Week and trap.rate columns
-  
+
   # required packages to run this function
   # library(MCMCpack)
   # library(lubridate)
-  # library(plyr)
+  # library(purrr)
   # library(dplyr)
   # library(tidyr)
   # library(magrittr)
-  
+
   # quick clean-up of column names
   trap.rate.df %<>%
-    rename(trap_open = trap.open)
-  
+    dplyr::rename(trap_open = trap.open)
+
   # R.V. of population mean run-timing given assigned hyper-parameters
   # R.V. of population mean run-timing given assigned hyper-parameters
   pop.mu <- rnorm(n.pops, run.mu.mu[1], run.mu.sd[1])
-  
+
   # R.V. of population standard deviation of run-timing given assigned hyper-parameters
   pop.sd <- abs(rnorm(n.pops, run.sd.mu[1], run.sd.sd[1]))
-  
+
   # LGR escapement by origin
   if(h.prob > 0){
     N.hat <- N.lgr * h.prob
     hat.pop.p <- rdirichlet(1, alpha = rbinom(n.pops,1,hatch.pop.prob))
       if(!any(is.na(hat.pop.p))){
-        N.hat.pop <- rmultinom(1, N.hat, hat.pop.p)  
+        N.hat.pop <- rmultinom(1, N.hat, hat.pop.p)
       } else {
         N.hat <- 0
         N.hat.pop <- rep(0,n.pops)
@@ -72,12 +72,12 @@ SimulateLGRdata = function(N.lgr = 100000,
     N.hat <- 0
     N.hat.pop <- rep(0,n.pops)
   }
-  
-  if(hnc.prob >0){  
+
+  if(hnc.prob >0){
     N.hnc <- N.lgr * hnc.prob
-    hnc.pop.p <- rdirichlet(1, alpha = rbinom(n.pops,1,hnc.pop.prob))
+    hnc.pop.p <- gtools::rdirichlet(1, alpha = rbinom(n.pops,1,hnc.pop.prob))
       if(!any(is.na(hnc.pop.p))){
-        N.hnc.pop <- rmultinom(1, N.hnc, hnc.pop.p)     
+        N.hnc.pop <- rmultinom(1, N.hnc, hnc.pop.p)
       } else {
         N.hnc <- 0
         N.hnc.pop <- rep(0,n.pops)
@@ -85,7 +85,7 @@ SimulateLGRdata = function(N.lgr = 100000,
   } else {
     N.hnc <- 0
     N.hnc.pop <- rep(0,n.pops)
-  } 
+  }
 
   N.wild <- N.lgr - (N.hat + N.hnc)
   wild.pop.p <- rdirichlet(1, alpha = rep(1, n.pops))
@@ -93,33 +93,36 @@ SimulateLGRdata = function(N.lgr = 100000,
 
   # LGR escapement by origin and population
   N.pop.origin <- cbind(N.wild.pop, N.hat.pop, N.hnc.pop)
-  
-  # True population escapement at Lower Granite Dam (includes all origins)  
+
+  # True population escapement at Lower Granite Dam (includes all origins)
   N.pop <- rowSums(N.pop.origin)
-  
+
   Origin <- NULL
   # NEED TO NOW SPLIT COLUMN VALUES INTO INDIVIDUAL RECORDS.
-  
+
   for(i in 1:n.pops){
-    tmp <- c(rep("NOR", N.pop.origin[i,1]), 
-             rep("HOR",N.pop.origin[i,2]), 
+    tmp <- c(rep("NOR", N.pop.origin[i,1]),
+             rep("HOR",N.pop.origin[i,2]),
              rep("HNC",N.pop.origin[i,3]))
     Origin <- c(Origin, tmp)
   }
-  
+
   # Empty list to collect passage dates of individuals by population
   obs.day <- list()
-  
+
   # Generate random day of passage for each population
   for(i in 1:sum(n.pops)){
     obs.day[[i]] <- round(rnorm(N.pop[i], pop.mu[i], pop.sd[i]), 0)
   }
-  
+
   names(obs.day) <- paste0("Pop-", 1:sum(n.pops))
-  
+
   # create data frame with row for each fish
-  LGR.df <- ldply(obs.day,data.frame, .id = 'Population') %>% tbl_df() %>%
-    rename(Day = `X..i..`) %>%
+  LGR.df <- purrr::map_df(obs.day,
+                          .id = 'Population',
+                          .f = function(x) {
+                            tibble(Day = x)
+                          }) %>%
     mutate(Week = week(ymd(paste0(year(start.date), '0101')) + days(Day)),
            Origin = factor(Origin,levels=c("NOR","HOR","HNC")),
            Run = ifelse(as.integer(gsub('Pop-', '', Population)) <= n.pops[1], 'Spring', 'Summer'),
@@ -137,7 +140,7 @@ SimulateLGRdata = function(N.lgr = 100000,
            id = 1:n()) %>%
     arrange(Day, Group, Origin) %>%
     dplyr::select(id, everything())
-  
+
   if(sum(LGR.df$Reascent) > 0) {
     LGR.reascents = LGR.df %>%
       filter(Reascent == 1) %>%
@@ -158,7 +161,7 @@ SimulateLGRdata = function(N.lgr = 100000,
       slice(1) %>%
       mutate_each(funs(ifelse(!is.na(.), NA, NA)))
   }
-  
+
   if(sum(LGR.reascents$Reascent, na.rm = T) > 0) {
     LGR.reascents_2 = LGR.reascents %>%
       filter(Reascent == 1) %>%
@@ -179,7 +182,7 @@ SimulateLGRdata = function(N.lgr = 100000,
       slice(1) %>%
       mutate_each(funs(ifelse(!is.na(.), NA, NA)))
   }
-  
+
   if(sum(LGR.reascents_2$Reascent, na.rm = T) > 0) {
     LGR.reascents_3 = LGR.reascents_2 %>%
       filter(Reascent == 1) %>%
@@ -200,7 +203,7 @@ SimulateLGRdata = function(N.lgr = 100000,
       slice(1) %>%
       mutate_each(funs(ifelse(!is.na(.), NA, NA)))
   }
-  
+
   LGR.df %<>%
     bind_rows(LGR.reascents) %>%
     bind_rows(LGR.reascents_2) %>%
@@ -212,8 +215,8 @@ SimulateLGRdata = function(N.lgr = 100000,
     ungroup() %>%
     select(-Date) %>%
     select(id, Day, Week, Start_Date, everything())
-  
-  
+
+
   return(list('sim' = LGR.df,
               'parameters' = pairlist(N.lgr = N.lgr,
                                       n.pops = n.pops,
@@ -231,7 +234,7 @@ SimulateLGRdata = function(N.lgr = 100000,
                                       ladder.det = ladder.det,
                                       start.date = start.date,
                                       trap.rate.df = trap.rate.df)))
-  
+
 }
 
 #-----------------------------------------------------------------
@@ -239,33 +242,39 @@ SimulateLGRdata = function(N.lgr = 100000,
 SummariseWeekly = function(sim_params,
                            lgr_truth,
                            samp.origin = 'NOR') {
-  
+
   # these parameters are used elsewhere as well as the simulation function
   night.passage.rate = sim_params$night.passage.rate
   window.rate = sim_params$window.rate
-  
+
   org_truth = lgr_truth %>%
     filter(Origin == samp.origin)
-  
+
   org_week = org_truth %>%
     group_by(Week) %>%
     summarise(N_tot = length(id),
-              N_uniq = n_distinct(id)) %>%
+              N_uniq = n_distinct(id),
+              .groups = "drop") %>%
     left_join(org_truth %>%
                 group_by(Week) %>%
-                summarise_each(funs(sum), Night.passage, Day.passage, Window.passage, Fallback, Reascent, Trap, Ladder) %>%
-                ungroup()) %>%
+                summarise(across(c(Night.passage, Day.passage, Window.passage, Fallback, Reascent, Trap, Ladder),
+                                 sum),
+                          .groups = "drop")) %>%
     left_join(org_truth %>%
                 filter(Marked == 1,
                        Ladder == 1) %>%
                 group_by(Week) %>%
-                summarise_each(funs(sum), Reascent, Night.passage) %>%
-                rename(mark_reasc = Reascent,
+                summarise(across(c(Reascent, Night.passage),
+                                 sum),
+                          .groups = "drop") %>%
+                dplyr::rename(mark_reasc = Reascent,
                        mark_night = Night.passage)) %>%
-    mutate_each(funs(ifelse(is.na(.), 0, .)), -Week) %>%
+    mutate(across(-Week,
+                  tidyr::replace_na,
+                  replace = 0)) %>%
     mutate(Origin = samp.origin) %>%
     select(Week, Origin, everything())
-  
+
   return(org_week)
 }
 
@@ -278,91 +287,105 @@ SimulateLGRobs = function(sim_params,
                           # theta = 100,
                           error_rate = 0.05,
                           perfect.window = F) {
-  
+
   window.rate = sim_params$window.rate
-  
+
   wild_week = SummariseWeekly(sim_params, lgr_truth, samp.origin = 'NOR')
   hatch_week = SummariseWeekly(sim_params, lgr_truth, samp.origin = 'HOR')
   hnc_week = SummariseWeekly(sim_params, lgr_truth, samp.origin = 'HNC')
-  
+
   lgr_week_org = wild_week %>%
     bind_rows(hatch_week) %>%
     bind_rows(hnc_week)
-  
+
   lgr_week_obs = lgr_week_org %>%
     group_by(Week) %>%
-    summarise_each(funs(sum), -Origin) %>%
+    summarise(across(-Origin,
+                     sum)) %>%
     left_join(lgr_truth %>%
                 select(Week, trap_open) %>%
                 distinct())
-  
+
   # simulate window counts. Add some observation error if !perfect.window
   if(perfect.window) {
     lgr_week_obs %<>%
       mutate(win_cnt = round(Window.passage / window.rate))
   }
-  
+
   # add observation error to daily window counts, then sum by week
   if(!perfect.window) {
     win_cnt_week = lgr_truth %>%
       group_by(Day, Week) %>%
-      summarise_each(funs(sum), Window.passage) %>%
-      ungroup() %>%
+      summarise(across(Window.passage,
+                       sum),
+                .groups = "drop") %>%
       mutate(win_cnt = round(rnorm(nrow(.), mean = Window.passage / window.rate, sd = error_rate * (Window.passage / window.rate))),
              # prevent counts from being negative
              win_cnt = ifelse(win_cnt < 0, 0, win_cnt)) %>%
       group_by(Week) %>%
-      summarise_each(funs(sum), win_cnt)
+      summarise(across(win_cnt,
+                       sum))
     lgr_week_obs %<>%
       left_join(win_cnt_week)
   }
-  
+
   # if(!perfect.window) {
   #   lgr_week_obs %>%
   #     mutate(win_cnt = rnegbin(nrow(.), mu = Window.passage / window.rate, theta = theta))
   # }
-  
+
   # mark-recapture estimate of trap rate
   trap_mr = lgr_truth %>%
     mutate(id = 1:n()) %>%
     filter(Marked == 1) %>%
     mutate(R = ifelse(Trap == 1 & Ladder == 1, 1, 0)) %>%
     filter(!(Trap == 0 & Ladder == 0)) %>%
-    select(id, 
+    select(id,
            Week,
            M = Trap,
            C = Ladder,
            R)
-  
+
   # put together list of capture histories
-  caphist_list = dlply(trap_mr, .(Week), capHistConvert, cols2use = c('M', 'C', 'R'), in.type = 'individual', out.type = 'frequency')
-  
+  caphist_list = trap_mr %>%
+    split(list(.$Week)) %>%
+    purrr::map(.f = capHistConvert,
+               cols2use = c('M', 'C', 'R'),
+               in.type = 'individual',
+               out.type = 'frequency')
+
   # estimate trap rate based on mark-recapture
-  trap_rate_mr = ldply(caphist_list, function(x) {
-    mod = try(closedp.t(x, dfreq=T), silent = T)
-    if(class(mod)[1] == 'try-error') {
-      return(data.frame(N_hat = NA, N_se = NA, p = NA, p_se = NA))
-    }
-    p_mean = inv.logit(coef(mod$glm[['Mt']])[2:3])
-    p_se = deltamethod(list(~ 1 / (1 + exp(-x1)), ~ 1 / (1 + exp(-x2))), mean = coef(mod$glm[['Mt']])[2:3], cov = vcov(mod$glm[['Mt']])[2:3,2:3])
-    return(data.frame(N_hat = mod$results['Mt', 'abundance'],
-                      N_se = mod$results['Mt', 'stderr'],
-                      p = p_mean[1], 
-                      p_se = p_se[1]))
-  }, .id = 'Week') %>% tbl_df()
-  
-  
+  trap_rate_mr = purrr::map_df(caphist_list,
+                               .id = 'Week',
+                               .f = function(x) {
+                                 mod = try(closedp.t(x, dfreq=T), silent = T)
+                                 if(class(mod)[1] == 'try-error') {
+                                   return(data.frame(N_hat = NA, N_se = NA, p = NA, p_se = NA))
+                                 }
+                                 p_mean = inv.logit(coef(mod$glm[['Mt']])[2:3])
+                                 p_se = deltamethod(list(~ 1 / (1 + exp(-x1)), ~ 1 / (1 + exp(-x2))), mean = coef(mod$glm[['Mt']])[2:3], cov = vcov(mod$glm[['Mt']])[2:3,2:3])
+                                 return(tibble(N_hat = mod$results['Mt', 'abundance'],
+                                               N_se = mod$results['Mt', 'stderr'],
+                                               p = p_mean[1],
+                                               p_se = p_se[1]))
+                               }) %>%
+    as_tibble() %>%
+    mutate(across(Week,
+                  as.integer)) %>%
+    arrange(Week)
+
   # add info from trap
   lgr_week_obs %<>%
     left_join(lgr_week_org %>%
                 group_by(Week, Origin) %>%
-                summarise(n_fish = sum(Trap)) %>%
-                ungroup() %>%
-                mutate(Origin = factor(Origin, levels = c('HOR', 'HNC', 'NOR')),
-                       Origin = revalue(Origin,
-                                        c('NOR' = 'wild_fish',
-                                          'HOR' = 'hatch_fish',
-                                          'HNC' = 'HNC_fish'))) %>%
+                summarise(n_fish = sum(Trap),
+                          .groups = "drop") %>%
+                mutate(Origin = factor(Origin,
+                                       levels = c('HOR', 'HNC', 'NOR')),
+                       Origin = recode(Origin,
+                                       'NOR' = 'wild_fish',
+                                       'HOR' = 'hatch_fish',
+                                       'HNC' = 'HNC_fish')) %>%
                 spread(Origin, n_fish, fill = 0))
   if(!'wild_fish' %in% names(lgr_week_obs)) lgr_week_obs %<>%
     mutate(hatch_fish = 0)
@@ -370,9 +393,11 @@ SimulateLGRobs = function(sim_params,
     mutate(hatch_fish = 0)
   if(!'HNC_fish' %in% names(lgr_week_obs)) lgr_week_obs %<>%
     mutate(HNC_fish = 0)
-  
+
   lgr_week_obs %<>%
-    mutate_each(funs(ifelse(is.na(.), 0, .)), Ladder:HNC_fish) %>%
+    mutate(across(c(Ladder:HNC_fish),
+                  tidyr::replace_na,
+                  replace = 0)) %>%
     left_join(trap_rate_mr %>%
                 select(Week,
                        trap_rate = p,
@@ -385,7 +410,7 @@ SimulateLGRobs = function(sim_params,
            trap_alpha = ifelse(trap_open, trap_alpha, 1e-12),
            trap_beta = ifelse(trap_open, trap_beta, 1)) %>%
     select(Week:Window.passage, win_cnt, everything())
-  
+
   return(list('by_origin' = lgr_week_org,
               'obs' = lgr_week_obs))
 }
@@ -395,11 +420,11 @@ SimulateLGRobs = function(sim_params,
 formatSCOBI_inputs <- function(weekly_obs, LGR_truth){
   # window_obs = output from SimulateLGRobs function; object$obs
   # LGR_truth = output from SimulateLGRdata function; obeject$sim
-  
+
   win_cnt_tmp = weekly_obs %>%
     select(WeekNumber = Week,
-           Counts = win_cnt) 
-    
+           Counts = win_cnt)
+
   fish_data <- LGR_truth %>%
     filter(Trap == 1) %>%
     select(WeekNumber = Week, Rear = Origin, GenStock = Population) %>%
@@ -407,7 +432,7 @@ formatSCOBI_inputs <- function(weekly_obs, LGR_truth){
                                   "HOR" = "H",
                                   "HNC" = "HNC"))) %>%
     arrange(WeekNumber)
-    
+
   window_count <- fish_data %>%
     group_by(WeekNumber,Rear) %>%
     summarise(n = n()) %>%
@@ -420,17 +445,17 @@ formatSCOBI_inputs <- function(weekly_obs, LGR_truth){
            Cumsum = cumsum(W_age),
            Collaps = cumsum(W_age >= 100)+1L)  %>%  # Does not collapse following IDFG's rule set.
     select(Strata = WeekNumber, Counts, Collaps)
-  
+
   return(list('window_count' = as.data.frame(window_count),
               'fish_data' = as.data.frame(fish_data)))
 }
 
 negbin_theta <- function(mu, error.rate) {
-  
+
   # mu = window.passage / window.rate
   # error.rate = coefficient of variation proportion, ie. 0.05, 0.15
-  
-  theta = mu^2 / ((error.rate*mu)^2 - mu) 
+
+  theta = mu^2 / ((error.rate*mu)^2 - mu)
 
   return(theta)
 }
